2215949aa812e604518106b162e3504d899fad6f
Vinayak Mehta <vmehta94@gmail.com>
2024-12-06 14:13:49 +0100

    Add version arg

--- src/houseplant/cli.py
+++ src/houseplant/cli.py
@@ -6,13 +6,35 @@ from typing import Optional
 import typer
 from rich.console import Console
 
-from houseplant import Houseplant
+from houseplant import __version__, Houseplant
 
-app = typer.Typer()
+
+app = typer.Typer(add_completion=False, no_args_is_help=True)
 
 
 def get_houseplant() -> Houseplant:
     return Houseplant()
+
+
+def version_callback(value: bool):
+    if value:
+        console = Console()
+        console.print(f"houseplant version {__version__}")
+        raise typer.Exit()
+
+
+@app.callback()
+def common(
+    version: Optional[bool] = typer.Option(
+        None,
+        "--version",
+        "-v",
+        help="Show version and exit.",
+        callback=version_callback,
+        is_eager=True,
+    ),
+):
+    pass
 
 
 @app.command()
@@ -22,11 +44,25 @@ def init():
     hp.init()
 
 
+@app.command(name="generate")
+def generate(name: str):
+    """Generate a new migration."""
+    hp = get_houseplant()
+    hp.generate(name)
+
+
 @app.command(name="migrate:status")
 def migrate_status():
     """Show status of database migrations."""
     hp = get_houseplant()
     hp.migrate_status()
+
+
+@app.command(name="migrate")
+def migrate(version: Optional[str] = typer.Argument(None)):
+    """Run migrations up to specified version."""
+    hp = get_houseplant()
+    hp.migrate(version)
 
 
 @app.command(name="migrate:up")
@@ -43,20 +79,6 @@ def migrate_down(version: Optional[str] = typer.Argument(None)):
     hp = get_houseplant()
     version = version or os.getenv("VERSION")
     hp.migrate_down(version)
-
-
-@app.command(name="migrate")
-def migrate(version: Optional[str] = typer.Argument(None)):
-    """Run migrations up to specified version."""
-    hp = get_houseplant()
-    hp.migrate(version)
-
-
-@app.command(name="generate")
-def generate(name: str):
-    """Generate a new migration."""
-    hp = get_houseplant()
-    hp.generate(name)
 
 
 @app.command(hidden=True)
